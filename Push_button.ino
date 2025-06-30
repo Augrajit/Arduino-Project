d12e95e0cd78bf7905f31fba937eb02783ad1b74
Augrajit <augrachatterjee@gmail.com>
2025-06-30 20:44:57 +0600

    Add files via upload

---
+++ Push_button.ino
@@ -0,0 +1,34 @@
+int sw=12,light=13;
+void setup() {
+   pinMode(sw,INPUT_PULLUP);
+   pinMode(light,OUTPUT);
+   Serial.begin(9600);
+}
+
+void loop() {
+  int r=button_press();
+  if(r!=0) Serial.println(r);
+
+}
+//Button press,sw=1; not pressed sw=0
+int button_press(){
+  int count=0;
+  p: int t=0;
+  if(digitalRead(sw)==0){
+    digitalWrite(light,1);
+    while(digitalRead(sw)==0){
+      delay(1);t++;
+    }
+    digitalWrite(light,0);
+    if(t>15){
+      count ++;
+      t=0;//Make the function wait for the next push 
+      while(digitalRead(sw)==1){
+        delay(1);t++;
+        if (t>1000) return count;
+      }
+      goto p;
+    }
+  }
+  return count;//If not pressed count=0
+}
